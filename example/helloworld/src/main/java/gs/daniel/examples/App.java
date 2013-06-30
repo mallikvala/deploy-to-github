d244d237fae340f424ed923d4f68439c6fffefe3
Daniel Kang <me@daniel.gs>
2013-06-30 01:22:55 -0700

    example projects added

---
+++ example/helloworld/src/main/java/gs/daniel/examples/App.java
@@ -0,0 +1,11 @@
+package gs.daniel.examples;
+
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        final int sum = Math.add(5, 2);
+
+        System.out.println("Sum of 5 and 2 is " + sum);
+    }
+}
---
+++ example/mathlib/src/main/java/gs/daniel/examples/Math.java
@@ -0,0 +1,7 @@
+package gs.daniel.examples;
+
+public class Math {
+    public static int add(final int a, final int b) {
+    	return a + b;
+    }
+}
